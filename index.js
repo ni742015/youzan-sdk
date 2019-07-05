98806d0b2d35887f8fc7eecb2b48d1276c3f3bfb
Yohann <15221385503@163.com>
2019-07-05 19:23:33 +0800

    feat: 升级支持新版sdk

--- index.js
+++ index.js
@@ -2,24 +2,27 @@
 
 function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
 
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 // 本文件用于wechat API，基础文件，主要用于Token的处理和mixin机制
-var urllib = require('urllib');
+// var urllib = require('urllib')
+var axios = require('axios');
 var extend = require('util')._extend;
 var middleware = require('./middleware');
 
 var AccessToken = function AccessToken(accessToken, expireTime) {
-	if (!(this instanceof AccessToken)) {
-		return new AccessToken(accessToken, expireTime);
-	}
-	this.accessToken = accessToken;
-	this.expireTime = expireTime;
+  if (!(this instanceof AccessToken)) {
+    return new AccessToken(accessToken, expireTime);
+  }
+  this.accessToken = accessToken;
+  this.expireTime = expireTime;
 };
 
 /*!
  * 检查AccessToken是否有效，检查规则为当前时间和过期时间进行对比
  */
 var validToken = function validToken(token) {
-	return !!token && !!token.accessToken && new Date().getTime() < token.expireTime;
+  return !!token && !!token.accessToken && new Date().getTime() < token.expireTime;
 };
 
 /**
@@ -27,26 +30,32 @@ var validToken = function validToken(token) {
  */
 
 var API = function API(_ref, getToken, saveToken) {
-	var client_id = _ref.client_id,
-	    client_secret = _ref.client_secret,
-	    kdt_id = _ref.kdt_id;
+  var client_id = _ref.client_id,
+      client_secret = _ref.client_secret,
+      authorize_type = _ref.authorize_type,
+      payload = _ref.payload;
 
-	this.client_id = client_id;
-	this.client_secret = client_secret;
-	this.kdt_id = kdt_id;
-	this.getToken = getToken || function () {
-		var token = this.store;
-		return validToken(token) && token;
-	};
-	this.saveToken = saveToken || function (token) {
-		this.store = token;
-		if (process.env.NODE_ENV === 'production') {
-			console.warn('Don\'t save token in memory, when cluster or multi-computer!');
-		}
-		return token;
-	};
-	this.prefix = 'https://open.youzan.com/';
-	this.defaults = {};
+  this.client_id = client_id;
+  this.client_secret = client_secret;
+  this.authorize_type = authorize_type;
+  this.payload = payload;
+
+  this.getToken = getToken || function () {
+    var token = this.store;
+    return validToken(token) && token;
+  };
+  this.saveToken = saveToken || function (token) {
+    this.store = token;
+    if (process.env.NODE_ENV === 'production') {
+      console.warn("Don't save token in memory, when cluster or multi-computer!");
+    }
+    return token;
+  };
+  // this.prefix = 'https://open.youzan.com/'
+  this.prefix = 'https://open.youzanyun.com';
+  this.defaults = {};
+  axios.defaults.baseURL = this.prefix;
+  axios.defaults.headers.post['User-Agent'] = 'YZY-Open-Client 1.0.0 - Node';
 };
 
 /**
@@ -59,7 +68,8 @@ var API = function API(_ref, getToken, saveToken) {
  * @param {Object} opts 默认选项
  */
 API.prototype.setOpts = function (opts) {
-	this.defaults = opts;
+  this.defaults = opts;
+  extend(axios.defaults, opts);
 };
 
 /**
@@ -73,52 +83,83 @@ API.prototype.setOpts = function (opts) {
  * ```
  * @param {Function} beforeRequest 需要封装的方法
  */
-API.prototype.request = function (url) {
-	var opts = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+API.prototype.request = function () {
+  var opts = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
 
-	var options = {};
-	extend(options, this.defaults);
-	for (var key in opts) {
-		if (key !== 'headers') {
-			options[key] = opts[key];
-		} else {
-			if (opts.headers) {
-				options.headers = options.headers || {};
-				extend(options.headers, opts.headers);
-			}
-		}
-	}
-	return urllib.request(url, options);
+  var options = {};
+  extend(options, this.defaults);
+  for (var key in opts) {
+    if (key !== 'headers') {
+      options[key] = opts[key];
+    } else {
+      if (opts.headers) {
+        options.headers = options.headers || {};
+        extend(options.headers, opts.headers);
+      }
+    }
+  }
+  //   console.log(options)
+
+  return axios.request(options);
 };
 
 /*!
  * 根据创建API时传入的账号获取access token
  */
 API.prototype.getAccessToken = async function (ifForce) {
-	var token = await this.getToken();
-	if (!token || ifForce) {
-		var url = this.prefix + 'oauth/token?grant_type=silent&client_id=' + this.client_id + '&client_secret=' + this.client_secret + '&kdt_id=' + this.kdt_id;
+  try {
+    var token = await this.getToken();
+    var client_id = this.client_id,
+        client_secret = this.client_secret,
+        authorize_type = this.authorize_type,
+        payload = this.payload;
 
-		var data = await this.request(url, { dataType: 'json' }).then(function (res) {
-			return res.data;
-		});
+    var attr = {
+      silent: 'grant_id',
+      authorization_code: 'redirect_uri',
+      refresh_token: 'refresh_token'
+    }[authorize_type];
+    if (!token || ifForce) {
+      var url = '/auth/token';
 
-		// 过期时间，因网络延迟等，将实际过期时间提前10秒，以防止临界点
-		var expireTime = new Date().getTime() + (data.expires_in - 10) * 1000;
-		token = this.saveToken(AccessToken(data.access_token, expireTime));
-	}
-	return token;
+      var res = await axios.create({
+        headers: {
+          'Content-type': 'application/json;charset=UTF-8'
+        }
+      }).post(url, _defineProperty({
+        client_id: client_id,
+        client_secret: client_secret,
+        authorize_type: authorize_type
+      }, attr, payload)).then(function (res) {
+        return res.data;
+      });
+      //   console.log(res)
+      if (res.success) {
+        var data = res.data;
+        // 过期时间，因网络延迟等，将实际过期时间提前10秒，以防止临界点
+
+        var expireTime = new Date().getTime() + (data.expires - 10) * 1000;
+        token = this.saveToken(AccessToken(data.access_token, expireTime));
+      } else {
+        throw res;
+        // throw new Error(res.message)
+      }
+    }
+    return token;
+  } catch (error) {
+    console.warn('get AccessToken error:', error);
+  }
 };
 
 /*!
  * 根据创建API时传入的账号获取access token
  */
 API.prototype.refreshToken = function () {
-	var _this = this;
+  var _this = this;
 
-	return this.getAccessToken(true).then(function (token) {
-		return _this.saveToken(token);
-	});
+  return this.getAccessToken(true).then(function (token) {
+    return _this.saveToken(token);
+  });
 };
 
 /**
@@ -137,45 +178,53 @@ API.prototype.refreshToken = function () {
  * @param {Array} args 方法需要的参数
  */
 API.prototype.invoke = async function (apiName) {
-	var _this2 = this;
+  var _this2 = this;
 
-	var opt = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
-	var _opt$version = opt.version,
-	    version = _opt$version === undefined ? '3.0.0' : _opt$version,
-	    _opt$dataType = opt.dataType,
-	    dataType = _opt$dataType === undefined ? 'json' : _opt$dataType;
+  var opt = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : {};
+  var _opt$version = opt.version,
+      version = _opt$version === undefined ? '3.0.0' : _opt$version,
+      _opt$responseType = opt.responseType,
+      responseType = _opt$responseType === undefined ? 'json' : _opt$responseType,
+      _opt$method = opt.method,
+      method = _opt$method === undefined ? 'POST' : _opt$method;
 
-	var args = arguments;
-	var url = this.prefix;
-	var service = apiName.substring(0, apiName.lastIndexOf('.'));
-	var action = apiName.substring(apiName.lastIndexOf('.') + 1, apiName.length);
-	var token = await this.getAccessToken();
+  var args = arguments;
+  var url = this.prefix;
+  //   var service = apiName.substring(0, apiName.lastIndexOf('.'))
+  //   var action = apiName.substring(apiName.lastIndexOf('.') + 1, apiName.length)
+  var token = await this.getAccessToken();
 
-	url += 'api/oauthentry/' + service + '/' + version + '/' + action + '?access_token=' + token.accessToken;
+  url += '/api/' + apiName + '/' + version + '?access_token=' + token.accessToken;
 
-	console.log('url, data', url, opt.data);
-	return this.request(url, extend({ dataType: dataType }, opt)).then(function (res) {
-		var data = res.data;
-		var error_response = data.error_response;
+  // console.log('url, data', url, opt.data)
+  return this.request(extend({ url: url, responseType: responseType, method: method }, opt)).then(function (res) {
+    // var data = res.data
+    var _res$data = res.data,
+        gw_err_resp = _res$data.gw_err_resp,
+        data = _res$data.data;
+    // console.log(gw_err_resp);
 
-		// 无效token重试
+    // 无效token重试
 
-		if (error_response) {
-			if ([40009, 40010].indexOf(error_response.code) >= 0) {
-				// console.log('error_response', data.error_response.code);
-				return _this2.refreshToken().then(function () {
-					return _this2.invoke.apply(_this2, _toConsumableArray(args));
-				});
-			} else {
-				var error = new Error('yzsdk invoke error: ' + error_response.code + ' - ' + error_response.msg);
-				error.code = error_response.code;
-				error.msg = error_response.msg;
-				throw error;
-			}
-		}
+    if (gw_err_resp) {
+      var err_code = gw_err_resp.err_code,
+          err_msg = gw_err_resp.err_msg;
 
-		return data.response || data.error_response;
-	});
+      if ([4201, 4202, 4203].indexOf(err_code) >= 0) {
+        // console.log('error_response', data.error_response.code);
+        return _this2.refreshToken().then(function () {
+          return _this2.invoke.apply(_this2, _toConsumableArray(args));
+        });
+      } else {
+        var error = new Error('yzsdk invoke error: ' + url + ', ' + JSON.stringify(opt) + ' - ' + err_code + ' - ' + err_msg);
+        error.code = err_code;
+        error.msg = err_msg;
+        throw error;
+      }
+    }
+
+    return data || gw_err_resp;
+  });
 };
 
 /**
@@ -188,12 +237,12 @@ API.prototype.invoke = async function (apiName) {
  * @param {Object} obj 要合并的对象
  */
 API.mixin = function (obj) {
-	for (var key in obj) {
-		if (API.prototype.hasOwnProperty(key)) {
-			throw new Error('Don\'t allow override existed prototype method. method: ' + key);
-		}
-		API.prototype[key] = obj[key];
-	}
+  for (var key in obj) {
+    if (API.prototype.hasOwnProperty(key)) {
+      throw new Error("Don't allow override existed prototype method. method: " + key);
+    }
+    API.prototype[key] = obj[key];
+  }
 };
 
 API.AccessToken = AccessToken;
--- src/index.js
+++ src/index.js
@@ -1,52 +1,62 @@
 // 本文件用于wechat API，基础文件，主要用于Token的处理和mixin机制
-var urllib = require('urllib')
+// var urllib = require('urllib')
+var axios = require('axios')
 var extend = require('util')._extend
 var middleware = require('./middleware')
 
 var AccessToken = function(accessToken, expireTime) {
-	if (!(this instanceof AccessToken)) {
-		return new AccessToken(accessToken, expireTime)
-	}
-	this.accessToken = accessToken
-	this.expireTime = expireTime
+  if (!(this instanceof AccessToken)) {
+    return new AccessToken(accessToken, expireTime)
+  }
+  this.accessToken = accessToken
+  this.expireTime = expireTime
 }
 
 /*!
  * 检查AccessToken是否有效，检查规则为当前时间和过期时间进行对比
  */
 var validToken = function(token) {
-	return (
-		!!token && !!token.accessToken && new Date().getTime() < token.expireTime
-	)
+  return (
+    !!token && !!token.accessToken && new Date().getTime() < token.expireTime
+  )
 }
 
 /**
  * 根据client_id、client_secret和kdt_id创建API的构造函数
  */
 
-var API = function({ client_id, client_secret, kdt_id }, getToken, saveToken) {
-	this.client_id = client_id
-	this.client_secret = client_secret
-	this.kdt_id = kdt_id
-	this.getToken =
-		getToken ||
-		function() {
-			var token = this.store
-			return validToken(token) && token
-		}
-	this.saveToken =
-		saveToken ||
-		function(token) {
-			this.store = token
-			if (process.env.NODE_ENV === 'production') {
-				console.warn(
-					'Don\'t save token in memory, when cluster or multi-computer!'
-				)
-			}
-			return token
-		}
-	this.prefix = 'https://open.youzan.com/'
-	this.defaults = {}
+var API = function(
+  { client_id, client_secret, authorize_type, payload },
+  getToken,
+  saveToken
+) {
+  this.client_id = client_id
+  this.client_secret = client_secret
+  this.authorize_type = authorize_type
+  this.payload = payload
+
+  this.getToken =
+    getToken ||
+    function() {
+      var token = this.store
+      return validToken(token) && token
+    }
+  this.saveToken =
+    saveToken ||
+    function(token) {
+      this.store = token
+      if (process.env.NODE_ENV === 'production') {
+        console.warn(
+          "Don't save token in memory, when cluster or multi-computer!"
+        )
+      }
+      return token
+    }
+  // this.prefix = 'https://open.youzan.com/'
+  this.prefix = 'https://open.youzanyun.com'
+  this.defaults = {}
+  axios.defaults.baseURL = this.prefix
+  axios.defaults.headers.post['User-Agent'] = 'YZY-Open-Client 1.0.0 - Node'
 }
 
 /**
@@ -59,7 +69,8 @@ var API = function({ client_id, client_secret, kdt_id }, getToken, saveToken) {
  * @param {Object} opts 默认选项
  */
 API.prototype.setOpts = function(opts) {
-	this.defaults = opts
+  this.defaults = opts
+  extend(axios.defaults, opts)
 }
 
 /**
@@ -73,51 +84,74 @@ API.prototype.setOpts = function(opts) {
  * ```
  * @param {Function} beforeRequest 需要封装的方法
  */
-API.prototype.request = function(url, opts = {}) {
-	var options = {}
-	extend(options, this.defaults)
-	for (var key in opts) {
-		if (key !== 'headers') {
-			options[key] = opts[key]
-		} else {
-			if (opts.headers) {
-				options.headers = options.headers || {}
-				extend(options.headers, opts.headers)
-			}
-		}
-	}
-	return urllib.request(url, options)
+API.prototype.request = function(opts = {}) {
+  var options = {}
+  extend(options, this.defaults)
+  for (var key in opts) {
+    if (key !== 'headers') {
+      options[key] = opts[key]
+    } else {
+      if (opts.headers) {
+        options.headers = options.headers || {}
+        extend(options.headers, opts.headers)
+      }
+    }
+  }
+//   console.log(options)
+
+  return axios.request(options)
 }
 
 /*!
  * 根据创建API时传入的账号获取access token
  */
 API.prototype.getAccessToken = async function(ifForce) {
-	var token = await this.getToken()
-	if(!token || ifForce) {
-		var url =
-			this.prefix +
-			'oauth/token?grant_type=silent&client_id=' +
-			this.client_id +
-			'&client_secret=' +
-			this.client_secret +
-			'&kdt_id=' +
-			this.kdt_id
+  try {
+    var token = await this.getToken()
+    var { client_id, client_secret, authorize_type, payload } = this
+    var attr = {
+      silent: 'grant_id',
+      authorization_code: 'redirect_uri',
+      refresh_token: 'refresh_token'
+    }[authorize_type]
+    if (!token || ifForce) {
+      var url = '/auth/token'
 
-		var data = await this.request(url, { dataType: 'json' }).then(res => res.data)
-
-		// 过期时间，因网络延迟等，将实际过期时间提前10秒，以防止临界点
-		var expireTime = new Date().getTime() + (data.expires_in - 10) * 1000
-		token = this.saveToken(AccessToken(data.access_token, expireTime))
-	}
-	return token
+      var res = await axios
+        .create({
+          headers: {
+            'Content-type': 'application/json;charset=UTF-8'
+          }
+        })
+        .post(url, {
+          client_id,
+          client_secret,
+          authorize_type,
+          [attr]: payload
+        })
+        .then(res => res.data)
+      //   console.log(res)
+      if (res.success) {
+        var { data } = res
+        // 过期时间，因网络延迟等，将实际过期时间提前10秒，以防止临界点
+        var expireTime = new Date().getTime() + (data.expires - 10) * 1000
+        token = this.saveToken(AccessToken(data.access_token, expireTime))
+      } else {
+        throw res
+        // throw new Error(res.message)
+      }
+    }
+    return token
+  } catch (error) {
+    console.warn('get AccessToken error:', error)
+  }
 }
 
 /*!
  * 根据创建API时传入的账号获取access token
  */
 API.prototype.refreshToken = function() {
-	return this.getAccessToken(true).then(token => this.saveToken(token))
+  return this.getAccessToken(true).then(token => this.saveToken(token))
 }
 
 /**
@@ -136,43 +170,42 @@ API.prototype.refreshToken = function() {
  * @param {Array} args 方法需要的参数
  */
 API.prototype.invoke = async function(apiName, opt = {}) {
-	var {version = '3.0.0', dataType = 'json'} = opt
-	var args = arguments
-	var url = this.prefix
-	var service = apiName.substring(0, apiName.lastIndexOf('.'))
-	var action = apiName.substring(apiName.lastIndexOf('.') + 1, apiName.length)
-	var token = await this.getAccessToken()
+  var { version = '3.0.0', responseType = 'json', method = 'POST' } = opt
+  var args = arguments
+  var url = this.prefix
+  //   var service = apiName.substring(0, apiName.lastIndexOf('.'))
+  //   var action = apiName.substring(apiName.lastIndexOf('.') + 1, apiName.length)
+  var token = await this.getAccessToken()
 
-	url +=
-		'api/oauthentry/' +
-		service +
-		'/' +
-		version +
-		'/' +
-		action +
-		'?access_token='
-		+ token.accessToken
+  url +=
+    '/api/' + apiName + '/' + version + '?access_token=' + token.accessToken
 
-	console.log('url, data', url, opt.data)
-	return this.request(url, extend({dataType}, opt)).then(res => {
-		var data = res.data
-		var {error_response} = data
+  // console.log('url, data', url, opt.data)
+  return this.request(extend({ url, responseType, method }, opt)).then(res => {
+    // var data = res.data
+    var { gw_err_resp, data } = res.data
+	// console.log(gw_err_resp);
 	
-		// 无效token重试
-		if(error_response) {
-			if ([40009, 40010].indexOf(error_response.code) >= 0) {
-				// console.log('error_response', data.error_response.code);
-				return this.refreshToken().then(() => this.invoke(...args))
-			} else {
-				const error = new Error(`yzsdk invoke error: ${error_response.code} - ${error_response.msg}`)
-				error.code = error_response.code
-				error.msg = error_response.msg
-				throw error
-			}
-		}
+    // 无效token重试
+    if (gw_err_resp) {
+      const { err_code, err_msg } = gw_err_resp
+      if ([4201, 4202, 4203].indexOf(err_code) >= 0) {
+        // console.log('error_response', data.error_response.code);
+        return this.refreshToken().then(() => this.invoke(...args))
+      } else {
+        const error = new Error(
+          `yzsdk invoke error: ${url}, ${JSON.stringify(
+            opt
+          )} - ${err_code} - ${err_msg}`
+        )
+        error.code = err_code
+        error.msg = err_msg
+        throw error
+      }
+    }
 
-		return data.response || data.error_response
-	})
+    return data || gw_err_resp
+  })
 }
 
 /**
@@ -185,14 +218,14 @@ API.prototype.invoke = async function(apiName, opt = {}) {
  * @param {Object} obj 要合并的对象
  */
 API.mixin = function(obj) {
-	for (var key in obj) {
-		if (API.prototype.hasOwnProperty(key)) {
-			throw new Error(
-				'Don\'t allow override existed prototype method. method: ' + key
-			)
-		}
-		API.prototype[key] = obj[key]
-	}
+  for (var key in obj) {
+    if (API.prototype.hasOwnProperty(key)) {
+      throw new Error(
+        "Don't allow override existed prototype method. method: " + key
+      )
+    }
+    API.prototype[key] = obj[key]
+  }
 }
 
 API.AccessToken = AccessToken
